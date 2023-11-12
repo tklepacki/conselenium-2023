9a2741c33a02601f9c4efb67d394a9e327f8dbc3
Tomasz Klepacki <t.klepacki@thesignalgroup.com>
2023-11-12 21:51:36 +0100

    Adding update

--- src/test/java/part12/PostTest.java
+++ src/test/java/part12/PostTest.java
@@ -12,13 +12,10 @@ public class PostTest {
 
     @Test
     public void addPostTest() {
-
-        Post post = new Post().setTitle("TestTitle").setAuthor("TestAuthor");
-
-        Integer addedPostId = RestService.getPostsService().addPost(post).
+        Integer addedPostId = RestService.getPostsService().addPost(addPostBody).
                 then().
-                body("author", equalTo(post.getAuthor())).
-                body("title", equalTo(post.getTitle())).
+                body("author", equalTo(addPostBody.getAuthor())).
+                body("title", equalTo(addPostBody.getTitle())).
                 statusCode(201).
                 extract().
                 path("id");
@@ -26,29 +23,25 @@ public class PostTest {
         RestService.getPostsService().getPost(addedPostId).
                 then().
                 body("id", equalTo(addedPostId)).
-                body("author", equalTo(post.getAuthor())).
-                body("title", equalTo(post.getTitle())).
+                body("author", equalTo(addPostBody.getAuthor())).
+                body("title", equalTo(addPostBody.getTitle())).
                 statusCode(200);
     }
 
     @Test
     public void editPostTest() {
 
-        Post post = new Post().setTitle("TestTitle").setAuthor("TestAuthor");
-
-        Integer addedPostId = RestService.getPostsService().addPost(post).
+        Integer addedPostId = RestService.getPostsService().addPost(addPostBody).
                 then().
                 statusCode(201).
                 extract().
                 path("id");
 
-        Post updatedPost = new Post().setTitle("TestTitleUpdated").setAuthor("TestAuthorUpdated");
-
-        Integer updatedPostId = RestService.getPostsService().editPost(addedPostId, updatedPost).
+        Integer updatedPostId = RestService.getPostsService().editPost(addedPostId, editPostBody).
                 then().
                 body("id", equalTo(addedPostId)).
-                body("author", equalTo(updatedPost.getAuthor())).
-                body("title", equalTo(updatedPost.getTitle())).
+                body("author", equalTo(editPostBody.getAuthor())).
+                body("title", equalTo(editPostBody.getTitle())).
                 statusCode(200).
                 extract().
                 path("id");
@@ -56,17 +49,14 @@ public class PostTest {
         RestService.getPostsService().getPost(updatedPostId).
                 then().
                 body("id", equalTo(addedPostId)).
-                body("author", equalTo(updatedPost.getAuthor())).
-                body("title", equalTo(updatedPost.getTitle())).
+                body("author", equalTo(editPostBody.getAuthor())).
+                body("title", equalTo(editPostBody.getTitle())).
                 statusCode(200);
     }
 
     @Test
-    public void getPostListTest() {
-
-        Post post = new Post().setTitle("TestTitle").setAuthor("TestAuthor");
-
-        Integer addedPostId = RestService.getPostsService().addPost(post).
+    public void getPostTest() {
+        Integer addedPostId = RestService.getPostsService().addPost(addPostBody).
                 then().
                 statusCode(201).
                 extract().
@@ -75,17 +65,15 @@ public class PostTest {
         RestService.getPostsService().getPostList().
                 then().
                 body("find { it.id == " + addedPostId + " }.id", equalTo(addedPostId)).
-                body("find { it.id == " + addedPostId + " }.title", equalTo(post.getTitle())).
-                body("find { it.id == " + addedPostId + " }.author", equalTo(post.getAuthor())).
+                body("find { it.id == " + addedPostId + " }.title", equalTo(addPostBody.getTitle())).
+                body("find { it.id == " + addedPostId + " }.author", equalTo(addPostBody.getAuthor())).
                 statusCode(200);
     }
 
     @Test
     public void deletePostTest() {
 
-        Post post = new Post().setTitle("TestTitle").setAuthor("TestAuthor");
-
-        Integer addedPostId = RestService.getPostsService().addPost(post).
+        Integer addedPostId = RestService.getPostsService().addPost(addPostBody).
                 then().
                 statusCode(201).
                 extract().
@@ -98,49 +86,5 @@ public class PostTest {
         RestService.getPostsService().getPostList().
                 then().
                 body("id", not(hasItems(addedPostId)));
-    }
-
-    @Test
-    public void addPostBodyFromFileTest() {
-        Integer addedPostId = RestService.getPostsService().addPost(addPostBody).
-                then().
-                body("author", equalTo("TestAuthor")).
-                body("title", equalTo("TestTitle")).
-                statusCode(201).
-                extract().
-                path("id");
-
-        RestService.getPostsService().getPost(addedPostId).
-                then().
-                body("id", equalTo(addedPostId)).
-                body("author", equalTo("TestAuthor")).
-                body("title", equalTo("TestTitle")).
-                statusCode(200);
-    }
-
-    @Test
-    public void editPostBodyFromFileTest() {
-
-        Integer addedPostId = RestService.getPostsService().addPost(addPostBody).
-                then().
-                statusCode(201).
-                extract().
-                path("id");
-
-        Integer updatedPostId = RestService.getPostsService().editPost(addedPostId, editPostBody).
-                then().
-                body("id", equalTo(addedPostId)).
-                body("author", equalTo("TestAuthorUpdated")).
-                body("title", equalTo("TestTitleUpdated")).
-                statusCode(200).
-                extract().
-                path("id");
-
-        RestService.getPostsService().getPost(updatedPostId).
-                then().
-                body("id", equalTo(addedPostId)).
-                body("author", equalTo("TestAuthorUpdated")).
-                body("title", equalTo("TestTitleUpdated")).
-                statusCode(200);
     }
 }
