33ec094c55819f23aa723b2125d0392e9a419671
Tomasz Klepacki <t.klepacki@wp.pl>
2025-06-06 22:01:44 +0200

    Codebase finalization

--- src/test/java/part01/UserTest.java
+++ src/test/java/part01/UserTest.java
@@ -3,7 +3,6 @@ package part01;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
 
 public class UserTest {
 
--- src/test/java/part02/UserTest.java
+++ src/test/java/part02/UserTest.java
@@ -3,7 +3,6 @@ package part02;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class UserTest {
